53d9b415a084911eeb58b6f9a5b3ecd31de72fd2
Hamza Elkhoudiri <elkhoudh@gmail.com>
2018-12-09 17:30:56 -0500

    Dynamic Images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import snow from './icons/snowflake.png';
+import cloud from './icons/cloud.png';
+import rain from './icons/rain.png';
+import sunny from './icons/sunny.png';
+import windy from './icons/windy.png';
 import './App.css';
 
 class App extends Component {
   state = {
     long: '',
     lat: '',
-    data: ''
+    summary: '',
+    temperature: '',
+    icon: '',
+    iconLogo: ''
   }
 
   componentDidMount(){
@@ -20,21 +27,33 @@ class App extends Component {
     }
   }
 
+  displayIcon(icon){
+    if(icon === 'clear-day' || icon === 'clear-night'){
+      this.setState({iconLogo: sunny})
+    } else if(icon === 'rain') {
+      this.setState({iconLogo: rain})
+    } else if(icon === 'snow' || icon === 'sleet'){
+      this.setState({iconLogo: snow})
+    }else if(icon === 'wind') {
+      this.setState({iconLogo: windy})
+    } else {
+      this.setState({iconLogo: cloud})
+    }
+  }
   getWeather(long, lat){
     const proxyUrl = "https://cors-anywhere.herokuapp.com/",
           targetUrl = `https://api.darksky.net/forecast/d3f7f07e93930037b1e70c5e761871e5/${lat},${long}`
     fetch(proxyUrl + targetUrl)
       .then(res => res.json())
-      .then(data => this.setState({data: data.currently}))
+      .then(data => this.setState({summary: data.currently.summary, temperature: data.currently.temperature, icon: data.currently.icon}, this.displayIcon(data.currently.icon)))
   }
 
   render() {
     return (
       <div className="App">
-      <img src={snow} alt="..."></img>
-        <p>{this.state.data.summary}</p>
-        <p>{this.state.data.temperature}</p>
-        <p>{this.state.data.icon}</p>
+      <img src={this.state.iconLogo} alt="..."></img>
+        <p>{this.state.summary}</p>
+        <p>{this.state.temperature}</p>
       </div>
     );
   }
